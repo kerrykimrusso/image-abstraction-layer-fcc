c36d8bc4cea655e8e8034bf030282d9477eb3623
System <none>
2017-08-06 23:39:48 +0000

    Checkpoint

--- test/SearchQuery.integration.spec.js
+++ test/SearchQuery.integration.spec.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 const mocha = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -7,13 +5,6 @@ chai.use(chaiHttp);
 
 const app = require('../app')();
 const SearchQueryModel = require('../api/search/SearchQuery.model');
-
-mocha.before((done) => {
-  mongoose.connect(process.env.MONGOTESTDB).then(
-    done,
-    console.log.bind(console, 'aw man... integration test could not connect.')
-  )
-});
 
 mocha.describe('Endpoint /api/v1/imagesearch', () => {
   mocha.before((done) => {
