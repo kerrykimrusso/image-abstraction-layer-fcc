d5a0b2db5ea9f37ee9f2941681c1684db4cbe40f
System <none>
2017-08-06 16:36:48 +0000

    Checkpoint

--- test/SearchQuery.integration.spec.js
+++ test/SearchQuery.integration.spec.js
@@ -1,18 +1,25 @@
-const app = require('../app')();
+const mongoose = require('mongoose');
 const mocha = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
+const app = require('../app')();
+const SearchQueryModel = require('../api/search/SearchQuery.model');
+
 mocha.before((done) => {
   mongoose.connect(process.env.MONGOTESTDB).then(
     done,
-    
+    console.log.bind(console, 'integration test could not connect with following error:')
   )
 });
 
-mocha.describe(() => {
-  mocha.it(() => {
-    
+mocha.describe('Endpoint /api/v1/imagesearch', () => {
+  mocha.before((done) => {
+    SearchQueryModel.
+  });
+  
+  mocha.it('should list history of serach terms ordered by date in descending order', (done) => {
+    chai.
   });
 });
