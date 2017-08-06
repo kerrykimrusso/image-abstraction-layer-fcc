bdee091ee96e8a96b8c3f3ad3a6fc6d60b378cfa
System <none>
2017-08-06 16:25:48 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,22 +4,10 @@
 // init project
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB).then(() => {
-  const express = require('express');
-  const app = express();
 
-  // http://expressjs.com/en/starter/static-files.html
-  app.use(express.static('public'));
+const app = require('./app');
 
-  // http://expressjs.com/en/starter/basic-routing.html
-  app.get("/", function (request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+mongoose.connect(process.env.MONGODB).then(app, 
+  (err) => {
+    throw err; 
   });
-
-  // listen for requests :)
-  const listener = app.listen(process.env.PORT, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
-  });
-}, (err) => {
-  throw err; 
-});
