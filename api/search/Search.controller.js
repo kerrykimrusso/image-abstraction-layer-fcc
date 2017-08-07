26e20725c1f5a58ef087900d43a284cebd84fb8e
System <none>
2017-08-07 02:53:55 +0000

    Checkpoint

--- api/search/Search.controller.js
+++ api/search/Search.controller.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const https = require('https');
 const model = require('./Search.model');
 
 module.exports = {
@@ -22,19 +22,21 @@ module.exports = {
         });
   },
   getImages: function(req, res, next) {
-    let opts = {
-      host: "https://google.com",
-      path: "/search?q=" + req.params.query
-    }
-    
-    http.request(opts, (err, res) => {
-      if(err) next
+    https.get('https://google.com/search?q=' + encodeURIComponent(req.params.query), (err, res) => {
+      if(err) next();
       
+      let str = '';
+      res.on('data', (chunk) => {
+        str += chunk;
+      });
+      
+      res.on('end', (chunk) => {
+        res.send(str).end();
+      });
     });
     // {"url":"https://i.ytimg.com/vi/8nOsEEBrEuA/hqdefault.jpg",
     // "snippet":"LOLCats in All Fired Up lol Cats Rock Funny Cats - YouTube",
     // "thumbnail":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuWNpmeNmLlMNc0v32tK83j4eOrOPVS6M2kty-CVFzQTvbS9d9WdKfrg7E",
     // "context":"https://www.youtube.com/watch?v=8nOsEEBrEuA"}
-    res.send('getImages()').end();
   }
 }
